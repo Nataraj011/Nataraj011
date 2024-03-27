a3d3a2a472c6f589dbfbfd1f0889e296fcecb751
Nataraj011 <103993941+Nataraj011@users.noreply.github.com>
2024-03-27 09:58:16 +0530

    Add files via upload

---
+++ Prodapt/Reversestring.java
@@ -0,0 +1,14 @@
+package demopack;
+
+public class Reversestring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="nataraj";
+		for(int i=s.length()-1;i>=0;i--){
+			System.out.print(s.charAt(i));
+		}
+	}
+
+
+}
