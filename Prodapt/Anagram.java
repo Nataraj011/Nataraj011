e905258b17918704443db2b0c5c52d671f96e43a
Nataraj011 <103993941+Nataraj011@users.noreply.github.com>
2024-03-27 09:55:40 +0530

    Add files via upload

---
+++ Prodapt/Anagram.java
@@ -0,0 +1,36 @@
+package demopack;
+
+public class Anagram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="parliment";
+		String s2="partial ben";
+		int f=0;
+		for(int i=0;i<s2.length();i++) {
+			f=0;
+			for(int j=0;j<s1.length();j++) {
+				if(s2.charAt(i)==' ') {
+					f=1;
+					
+				}
+				else if(s2.charAt(i)==s1.charAt(j)) {
+					f=1;
+					break;
+					
+				}
+			}
+			if(f==0) {
+				System.out.print("Not a anagram");
+				break;
+			}
+		}
+		if(f==1){
+			System.out.print("anagram");
+		}
+
+	}
+}
+
+
+
