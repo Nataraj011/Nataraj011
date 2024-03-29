9e61da66e1f7dabdbc26d32cd6f9dfa685547807
Nataraj011 <103993941+Nataraj011@users.noreply.github.com>
2024-03-29 16:08:50 +0530

    Add files via upload

---
+++ Java/Capsule.java
@@ -0,0 +1,23 @@
+package demopack;
+
+public class Capsule
+
+{
+
+public static int nCapsules = 0;
+
+public double volume;
+
+public String screenText;
+
+public Capsule(double volume,String s){
+
+this.volume = volume;
+
+screenText = s;
+
+nCapsules++;
+
+}
+
+}
---
+++ Java/Ex11_6.java
@@ -0,0 +1,13 @@
+package demopack;
+
+public class Ex11_6
+
+{
+
+public int a,b;
+
+public int c = 2;
+
+public static int x = 6;
+
+}
